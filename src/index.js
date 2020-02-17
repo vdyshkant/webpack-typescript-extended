1bb00b578dfe7c03a7e1cad78bc7b0043e304f48
Resolute Support <develop@fastmail.co.uk>
2020-02-17 21:58:54 +0200

    added sample javascript code

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Post from './Post'
+import './styles/styles.css'
+import Logo from './assets/logo.png'
+
+const post = new Post('Webpack Post Title', Logo)
+
+console.log('Post to String:', post.toString())
