ed7b716734dc08adb51dfc2bd57488c2562aa73f
Resolute Support <develop@fastmail.co.uk>
2020-02-17 22:19:11 +0200

    commonly used code now handling with no doubles (webpack tracks it)

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,11 @@ module.exports = {
       '@': path.resolve(__dirname, 'src')
     }
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
   plugins: [
     new HTMLWebpackPluguin({
       template: './index.html'
