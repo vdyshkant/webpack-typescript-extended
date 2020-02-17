9dc074ed280f1d934bc0e0f416bd1bc5182a97ef
Resolute Support <develop@fastmail.co.uk>
2020-02-17 21:15:09 +0200

    added html-webpack-plugin and selected template for it

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HTMLWebpackPluguin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -9,5 +10,11 @@ module.exports = {
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new HTMLWebpackPluguin({
+      title: 'Custom Webpack Title',
+      template: './src/index.html'
+    })
+  ]
 }
