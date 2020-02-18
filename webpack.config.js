2f4b14a5a7811f70ddba3e307d6b013fd8fb8bc4
Resolute Support <develop@fastmail.co.uk>
2020-02-18 17:39:38 +0200

    added proposals support by @babel

--- webpack.config.js
+++ webpack.config.js
@@ -115,6 +115,9 @@ module.exports = {
           options: {
             presets: [
               '@babel/preset-env'
+            ],
+            plugins: [
+              '@babel/plugin-proposal-class-properties'
             ]
           }
         }
