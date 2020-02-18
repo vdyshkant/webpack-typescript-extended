181ee1191a06629a4f85ab9975c6dada72b136a3
Resolute Support <develop@fastmail.co.uk>
2020-02-18 16:53:17 +0200

    removed hash names from dev bundle

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,8 @@ const optimization = () => {
   return config
 }
 
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
@@ -35,7 +37,7 @@ module.exports = {
     analytics: './analytics.js'
   },
   output: {
-    filename: '[name].[hash].js',
+    filename: filename('js'),
     path: path.resolve(__dirname, 'dist')
   },
   resolve: {
@@ -66,7 +68,7 @@ module.exports = {
       }
     ]),
     new MiniCssExtractPlugin({
-      filename: '[name].[hash].css',
+      filename: filename('css'),
     })
   ],
   module: {
