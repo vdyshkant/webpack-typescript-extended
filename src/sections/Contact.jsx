040f4e1dda0f9a2742ad6c1e0982777214071fae
vdyshkant <develop@fastmail.co.uk>
2020-02-19 16:27:34 +0200

    refactoring

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -7,29 +7,34 @@ const FormItem = Form.Item
 const Contact = () => {
 
   return (
-    <div>
-      <h1>sample CONTACT section!</h1>
-      <div>
-        <Form.Item>
-          <Input
-            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-            placeholder="Username"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Input
-            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Log in
-          </Button>
-        </Form.Item>
+    <section className="section-contact">
+      <div className="container  container--contact">
+        <h1>sample CONTACT section!</h1>
+        <div>
+          <Form.Item>
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <a className="login-form-forgot" href="">Forgot password</a>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Log in
+            </Button>
+          </Form.Item>
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
 
